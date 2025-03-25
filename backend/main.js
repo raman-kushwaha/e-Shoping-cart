f6b7704a86ee457fb4dc805d5c52a85ff9450805
Raman kushwaha <ramankushwaha808@gmail.com>
2025-03-25 22:37:28 +0530

    updates

--- backend/main.js
+++ backend/main.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config();
+require("dotenv").config({ path: "./.env" });
 const fs = require("fs");
 const cookieParser = require("cookie-parser");
 
